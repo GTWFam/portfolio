33ba72143016e2e6953cd4d27450cceab21083fe
GTWFam <252704436go@gmail.com>
2021-10-19 19:54:41 -0400

    Running Python from node.js

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+const spawn = require('child_process').spawn;
 const express = require('express');
+
+const othello = spawn('python', ['./hello.py'])
+
+othello.stdout.on('data', data => {
+    console.log(data.toString())
+})
+
 const app = express();
 
 app.use(express.static('build'));
