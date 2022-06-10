e59cbf2cee37bae66f809ab182f9cc3488083d45
GTWFam <252704436go@gmail.com>
2022-06-10 11:32:29 -0400

    redeploying to heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
 const express = require("express");
 const mongodb = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
